1bb2ee37a48a8ea5a39973845f9cd6821d1513a5
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-03-22 14:33:03 +0530

    frontend build up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './Components/Header/Header';
+import AdminNotification from './Components/AdminNotification/AdminNotification';
+// import DeveloperNotification from './Components/DeveloperNotification/DeveloperNotification';
+// import CustomerNotification from './Components/CustomerNotification/CustomerNotification';
+
 
 function App() {
   return (
     <div className="App">
-     
+     <BrowserRouter>
+     <Routes>
+     <Route path = "/" exact element = {<Header/>}></Route>
+    
+        <Route path = "/admin" exact element = {<AdminNotification/>}></Route>
+        {/* <Route path = "/customer" exact element={<CustomerNotification/>}></Route>
+        <Route path = "/developer" exact element = {<DeveloperNotification/>}></Route> */}
+
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
